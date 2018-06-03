5363e5ffc8165cad455755175da9fde4f43ad598
Ekscursionist <larakek@gmail.com>
2018-06-03 11:06:13 -0400

    added images last time; notes pages filled; some basics

---
+++ assets/javascript/game.js
@@ -0,0 +1,44 @@
+// BULBASAUR
+// [my] HP - 100
+// [my] Att - 5 (++; each time)
+// [my] Cntr Att - 25
+
+// EEVEE
+// [my] HP - 160
+// [my] Att - 8
+// [my] Cntr Att - 40
+
+// MAREEP
+// [my] HP - 200
+// [my] Att - 10
+// [my] Cntr Att - 50
+
+// VULPIX
+// [my] HP - 220
+// [my] Att - 11
+// [my] Cntr Att - 55
+
+var bulbasaur = {
+    "hp" : 100,
+    "attack" : 5,
+    "counteratt" : 25,
+};
+
+var eevee = {
+    "hp" : 160,
+    "attack" : 8,
+    "counteratt" : 40,
+};
+
+var mareep = {
+    "hp" : 200,
+    "attack" : 10,
+    "counteratt" : 50,
+};
+
+var vulpix = {
+    "hp" : 220,
+    "attack" : 11,
+    "counteratt" : 55,
+};
+
