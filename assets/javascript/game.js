2446449b5c124e029bca67adc0574f21a15606d1
Ekscursionist <larakek@gmail.com>
2018-06-04 14:34:22 -0400

    setting up the array of pokechoices; general flailing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,14 +2,16 @@
 
 var userPoke; // I'm not sure if these should be empty or if they should be objects???
 var oppPoke; // I'm not sure if these should be empty or if they should be objects???
+var pokeChoices; // this will be an empty array that will fill with pokemon - 4 before game begins; 3 once userPoke is chosen
 
 var wins = 0;
 var losses = 1;
 
 
-function resetEverything {
+function resetEverything() {
 
-    pokeChoices = [
+    pokeChoices = [];
+    pokeArray = [
         {
             "name" : "bulbasaur",
             "hp" : 100,
@@ -18,7 +20,7 @@ function resetEverything {
             "frontpic" : 'assets/images/bulbasaur.png',
             "backpic" : 'assets/images/bulbaback.png',
         }, {
-            "name" : "eevee"
+            "name" : "eevee",
             "hp" : 160,
             "attack" : 8,
             "counteratt" : 40,
@@ -38,15 +40,20 @@ function resetEverything {
             "counteratt" : 55,
             "frontpic" : 'assets/images/vulpix.png',
             "backpic" : 'assets/images/vulpixback.png',
-        };
-    ]; // this is the closing ] for pokeChoices
+        } ]; // this is the closing ] for pokeArray
 
     chosePoke = false;
     choseEnemy = false;
+    numberofenemies = 3;
 
+    for (var i = 0; i < pokeArray.length; i++) {
+        pokeChoices.push (pokeArray.name);
+        console.log(pokeChoices);
+		};
+    
 
 //  choosePoke();    
-}
+};
 
 
 // ------------------------------------- vars & reset section finito -------------------------------------- //
@@ -60,34 +67,43 @@ function choosePoke() {
 }
 
 
-function winCondition() {
-    if // userPoke's HP is
-    wins++;
-    // chooseNextOpponent;
-}
+// function winCondition() {
+//     if // userPoke's HP is
+//     wins++;
+//     // chooseNextOpponent;
+// }
 
-function loseCondition() {
-    if (losses == 1) {
-    // userPoke's HP is <= 0,
-    losses++;
-    alert("You've lost! Choose a new pokemon to play again!");
-    // chooseNextOpponent; 
-}
-}
+// function loseCondition() {
+//     if (losses == 1) {
+//         // userPoke's HP is <= 0,
+//         losses++;
+//         alert("You've lost! Choose a new pokemon to play again!");
+//         // chooseNextOpponent; 
+//     }
+// }
 
-function bigWin() {
-    if (wins == 4) {
-        alert("YOU HAVE DEFEATED ALL ENEMY POKEMON!!");
-        // offerPlayAgain;
-    }
-};
+// function bigWin() {
+//     if (wins == 4) {
+//         alert("YOU HAVE DEFEATED ALL ENEMY POKEMON!!");
+//         // offerPlayAgain;
+//     }
+// };
 // ----------------------------- section for declaring functions finito ---------------------------- //
 
 // ---------------------------------- section for calling functions --------------------------------- //
 
-bigWin();
+$("#fourwholepokes").html(pokeChoices);
+
+
 
 // choosePoke();
+
+
+
+// bigWin();            // this function works, but I am commenting it out during building/testing
+// loseCondition();     // this function works, but I am commenting it out during building/testing
+// winCondition();      // this function works, but I am commenting it out during building/testing
+
 
 // ------------------------------ section for calling functions finito ------------------------------ //
 
