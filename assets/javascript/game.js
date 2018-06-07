c543bdf077d10b4d3f1654faca5bdd60fce4b52c
Ekscursionist <larakek@gmail.com>
2018-06-07 11:34:02 -0400

    are things getting fixed? yes.  are things getting better? um.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,8 +4,42 @@ $(document).ready(function() {
 var userPoke = {}; // 
 var oppPoke = {}; //
 
-var pokeChoices = []; // this will be an empty array that will fill with pokemon - 4 before game begins; 3 once userPoke is chosen
-var pokeArray = []; // this is the initial array of pokemon-objects
+var pokeChoices = [];
+var pokeArray = [
+    {
+        "name" : "bulbasaur",
+        id : 0,
+        "hp" : 100,
+        "attack" : 5,
+        "counteratt" : 25,
+        "frontpic" : 'assets/images/bulbasaur.png',
+        "backpic" : 'assets/images/bulbaback.png',
+    }, {
+        "name" : "eevee",
+        id : 1,
+        "hp" : 160,
+        "attack" : 8,
+        "counteratt" : 40,
+        "frontpic" : 'assets/images/eevee.png',
+        "backpic" : 'assets/images/eeveeback.png',
+    }, {
+        "name" : "mareep",
+        id : 2,
+        "hp" : 200,
+        "attack" : 10,
+        "counteratt" : 50,
+        "frontpic" : 'assets/images/mareep.png',
+        "backpic" : 'assets/images/mareepback.png',
+    }, {
+        "name" : "vulpix",
+        id : 3,
+        "hp" : 220,
+        "attack" : 11,
+        "counteratt" : 55,
+        "frontpic" : 'assets/images/vulpix.png',
+        "backpic" : 'assets/images/vulpixback.png',
+    } ]; // this is the closing ] for pokeArray
+
 
 var chosePoke; // this is the var for a user-poke being chosen or not ---------- boolean --------- ???set it yet or no?
 var choseEnemy; // this is the var for an enemy-poke being chosen or not ---------- boolean --------- ???set it yet or no?
@@ -22,6 +56,7 @@ var yourPoke = 0;
 var thisisyourpoke = false;
 var thisisyourenemy = false;
 
+
 function resetEverything() {
     userPoke = {};
     oppPoke = {};
@@ -31,43 +66,6 @@ function resetEverything() {
 
     $('.myPoke').remove();
     $('.yourPoke').remove(); //note: 'fighting'
-
-    pokeChoices = [];
-    pokeArray = [
-        {
-            "name" : "bulbasaur",
-            id : 0,
-            "hp" : 100,
-            "attack" : 5,
-            "counteratt" : 25,
-            "frontpic" : 'assets/images/bulbasaur.png',
-            "backpic" : 'assets/images/bulbaback.png',
-        }, {
-            "name" : "eevee",
-            id : 1,
-            "hp" : 160,
-            "attack" : 8,
-            "counteratt" : 40,
-            "frontpic" : 'assets/images/eevee.png',
-            "backpic" : 'assets/images/eeveeback.png',
-        }, {
-            "name" : "mareep",
-            id : 2,
-            "hp" : 200,
-            "attack" : 10,
-            "counteratt" : 50,
-            "frontpic" : 'assets/images/mareep.png',
-            "backpic" : 'assets/images/mareepback.png',
-        }, {
-            "name" : "vulpix",
-            id : 3,
-            "hp" : 220,
-            "attack" : 11,
-            "counteratt" : 55,
-            "frontpic" : 'assets/images/vulpix.png',
-            "backpic" : 'assets/images/vulpixback.png',
-        } ]; // this is the closing ] for pokeArray
-
     
     for (var i = 0; i < pokeArray.length; i++) {
        pokeChoices += "<div id=" + pokeArray[i].name + " class='btn character text-center' value=" + pokeArray[i].name +
@@ -76,6 +74,7 @@ function resetEverything() {
         console.log("this bit is working");
         };                      
                             // Note to self: I love how this created a div in js! brilliant!  This is going in my toolbox.
+                            // Second Note to Self: Update: I love it but I am going to use it sparingly, because it's kinda messy and I've learned a lot in the last three days
 
     $("#fourwholepokes").html(pokeChoices);
     $("#instructions").html("Choose a starter!");
@@ -89,9 +88,11 @@ function resetEverything() {
 
 
 
-// ----------------------------------------------- section for declaring functions -------------------------------------------------- //
+// ----------------------------------------------- functions time! -------------------------------------------------- //
 
-// something about this section of code isn't working.  I need to find out why. ------------------------------------ To Ask In Class
+resetEverything();
+choosePoke();
+
 function choosePoke() {
     $(".pokes").on("click", function() {
         $("#userChosenPoke").append(this);
@@ -104,8 +105,7 @@ function choosePoke() {
 
         if (thisisyourpoke == true) {
             chooseEnemy();
-        }
-        
+        }  
     }); // to onclick
 };
 
@@ -118,30 +118,23 @@ function chooseEnemy() {
         choseEnemy = true;
         thisisyourenemy = true;
         console.log("user has chosen an enemy!");
-        $("#instructions").html("You've chosen an enemy pokemon!  Prepare for battle!  <br> To battle your enemy pokemon, click Attack!");
+        $("#instructions").html("You've chosen an enemy pokemon!  Prepare for battle!  <br> To battle your enemy pokemon, click Pokebattle!");
+
+        if (thisisyourenemy == true) {
+            //hide other pokes?
+            battletime();
+        }  
     }); // to onclick
 };
 
-choosePoke();
+// choosePoke();
 
 
-        // if(chosePoke == false) {
-        //     userPoke = $(this).attr('id');
-        //     $("#userChosenPoke").append(this);
-        //     $(this).addClass("myPoke");
-        //     chosePoke = true;
-        //     console.log("user has chosen a poke!");
-        //     $('#battledescrip').html("");
-        //     $("#instructions").html("Now choose the first pokemon you'd like to battle!");
-        // } else {
-        //     oppPoke = $(this).attr('id');
-        //     $("#userChosenEnemy").append(this);
-        //     $(this).addClass("yourPoke");    
-        //     choseEnemy = true;
-        //     console.log("user has chosen first enemy!")
-        //     $('#battledescrip').html("");
-        //     $("#instructions").html("Your pokebattle is about to begin! Click attack!");
-//        }
+function battletime() { console.log("battling")
+    description = "let's try this"
+    $("instructions").html(description);
+}
+
 
 
 function offerPlayAgain() {
@@ -160,9 +153,6 @@ function bigWin() {
 }
 
 
-function battletime() {
-
-}
 
 
 
@@ -181,8 +171,6 @@ function battletime() {
 
 
 
-resetEverything();
-choosePoke();
 
 }); // to Document Ready Function
 
